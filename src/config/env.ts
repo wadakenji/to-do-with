408946ff165f03b1afcfb4b326006423a75531ea
wadakenji <ponporopen@gmail.com>
2022-11-01 18:50:34 +0900

    feat: supabaseSDK導入

---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  SUPABSE_PROJECT_URL: import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  SUPABSE_API_KEY: import.meta.env.VITE_SUPABASE_API_KEY,
+};
---
+++ src/lib/supabase/index.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+import { ENV } from '../../config/env';
+
+export const supabase = createClient(ENV.SUPABSE_PROJECT_URL, ENV.SUPABSE_API_KEY, {
+  auth: { autoRefreshToken: true },
+});
