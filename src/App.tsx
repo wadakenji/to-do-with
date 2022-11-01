28b85dd9f1c6239605ee6efd9f89972fa3454b80
wadakenji <ponporopen@gmail.com>
2022-11-01 16:58:16 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import Layout from './components/Layout';
+
+const App = () => {
+  return <Layout>hello</Layout>;
+};
+
+export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React, { PropsWithChildren } from 'react';
+import { Layout as AntdLayout } from 'antd';
+
+const { Header, Content } = AntdLayout;
+
+const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <AntdLayout>
+      <Header>
+        <div style={{ color: 'white', fontSize: 20 }}>いつかやりたい</div>
+      </Header>
+      <Content style={{ padding: '20px 40px 0 40px', minHeight: '100vh' }}>{children}</Content>
+    </AntdLayout>
+  );
+};
+
+export default Layout;
