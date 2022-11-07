9f615087c5b71e23520dc8cb69052ba5d967a212
wadakenji <ponporopen@gmail.com>
2022-11-07 16:41:16 +0900

    feat: Radiosコンポーネントのprop追加
    margin-bottomを外から設定する用

--- src/components/Todo/Radios.tsx
+++ src/components/Todo/Radios.tsx
@@ -4,17 +4,20 @@ import React from 'react';
 type Props = {
   radioState: RadioState;
   onRadioChange: (e: RadioChangeEvent) => void;
+  mb?: number;
 };
 
 export const Radios: React.FC<Props> = ({
   radioState,
   onRadioChange,
+  mb,
 }) => {
   return (
     <Radio.Group
       value={radioState}
       onChange={onRadioChange}
       buttonStyle="solid"
+      style={{ marginBottom: mb }}
     >
       <Radio.Button value="WANT_TO_DO">やりたい</Radio.Button>
       <Radio.Button value="DONE">やった</Radio.Button>
