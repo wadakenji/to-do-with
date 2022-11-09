1705754ef34d2ab731ec96a73989dc4a963617fe
wadakenji <ponporopen@gmail.com>
2022-11-09 14:55:41 +0900

    refactor: supabaseクライアントのジェネリクスを指定

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { ENV } from '../../config/env';
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   ENV.SUPABSE_PROJECT_URL,
   ENV.SUPABSE_API_KEY,
   {
