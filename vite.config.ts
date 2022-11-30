6daf1ca5fcb35d44f75d7a6e2f5d1729a3e12084
wadakenji <ponporopen@gmail.com>
2022-11-30 10:02:08 +0900

    feat: pwa設定テスト

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,18 @@ export default defineConfig({
       manifest: {
         name: 'いつかやりたい',
         short_name: 'いつかやりたい',
+        display: 'standalone',
+        background_color: '#f0f2f5',
+        icons: [
+          {
+            src: 'https://www.shutterstock.com/shutterstock/photos/1798019332/display_1500/stock-vector-free-sample-outline-glyph-icon-1798019332.jpg',
+            size: '192x192',
+          },
+          {
+            src: 'https://www.shutterstock.com/shutterstock/photos/1798019332/display_1500/stock-vector-free-sample-outline-glyph-icon-1798019332.jpg',
+            size: '512x512',
+          },
+        ],
       },
     }),
   ],
